459cf7c74d98df97563fffc21ae3145b56078cc9
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-09 14:48:36 +0545

    command to install blog package

---
+++ src/Console/InstallBlogPackage.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Raajkumarpaneru\BlogPackage\Console;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\File;
+
+class InstallBlogPackage extends Command
+{
+    protected $signature = 'blogpackage:install';
+
+    protected $description = 'Install the BlogPackage';
+
+    public function handle()
+    {
+        $this->info('Installing BlogPackage...');
+
+        $this->info('Publishing configuration...');
+
+        if (! $this->configExists('blogpackage.php')) {
+            $this->publishConfiguration();
+            $this->info('Published configuration');
+        } else {
+            if ($this->shouldOverwriteConfig()) {
+                $this->info('Overwriting configuration file...');
+                $this->publishConfiguration($force = true);
+            } else {
+                $this->info('Existing configuration was not overwritten');
+            }
+        }
+
+        $this->info('Installed BlogPackage');
+    }
+
+    private function configExists($fileName)
+    {
+        return File::exists(config_path($fileName));
+    }
+
+    private function shouldOverwriteConfig()
+    {
+        return $this->confirm(
+            'Config file already exists. Do you want to overwrite it?',
+            false
+        );
+    }
+
+    private function publishConfiguration($forcePublish = false)
+    {
+        $params = [
+            '--provider' => "JohnDoe\BlogPackage\BlogPackageServiceProvider",
+            '--tag' => "config"
+        ];
+
+        if ($forcePublish === true) {
+            $params['--force'] = true;
+        }
+
+        $this->call('vendor:publish', $params);
+    }
+}
