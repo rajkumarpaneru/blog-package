e9908a2fdec81b7d33db21332e383e1ffa9b20d2
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-09 14:10:44 +0545

    add service provider for package

---
+++ src/BlogPackageServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Raajkumarpaneru\BlogPackage;
+
+use Illuminate\Support\ServiceProvider;
+
+class BlogPackageServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
