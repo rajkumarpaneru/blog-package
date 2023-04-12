673ae7e4b96d5f97656aa3878fa423e0eb4e6191
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-12 21:36:08 +0545

    model and migration for posts

---
+++ src/Models/Post.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Raajkumarpaneru\BlogPackage\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Post extends Model
+{
+    use HasFactory;
+
+    // Disable Laravel's mass assignment protection
+    protected $guarded = [];
+}
