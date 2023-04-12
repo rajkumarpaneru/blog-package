ab2af817b4b04f0e053e26c7c65f01778ad1def9
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-12 21:27:37 +0545

    exporting config file

--- src/BlogPackageServiceProvider.php
+++ src/BlogPackageServiceProvider.php
@@ -14,11 +14,15 @@ class BlogPackageServiceProvider extends ServiceProvider
 
     public function boot()
     {
-        // Register the command if we are using the application via the CLI
         if ($this->app->runningInConsole()) {
             $this->commands([
                 InstallBlogPackage::class,
             ]);
+
+            $this->publishes([
+                __DIR__.'/../config/config.php' => config_path('blogpackage.php'),
+            ], 'config');
         }
+
     }
 }
