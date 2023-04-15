105931301608661955cb1eddb38860a68ae5b6c9
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-15 14:23:53 +0545

    tests to check that user can crate a post

--- tests/Unit/PostTest.php
+++ tests/Unit/PostTest.php
@@ -5,6 +5,7 @@ namespace Raajkumarpaneru\BlogPackage\Tests\Unit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Raajkumarpaneru\BlogPackage\Tests\TestCase;
 use Raajkumarpaneru\BlogPackage\Models\Post;
+use Raajkumarpaneru\BlogPackage\Tests\User;
 
 class PostTest extends TestCase
 {
@@ -37,5 +38,28 @@ class PostTest extends TestCase
     {
         $post = Post::factory()->create(['author_type' => 'Fake\User']);
         $this->assertEquals('Fake\User', $post->author_type);
+    }
+
+    /** @test */
+    function a_post_belongs_to_an_author()
+    {
+        // Given we have an author
+        $author = User::factory()->create();
+        // And this author has a Post
+        $author->posts()->create([
+            'title' => 'My first fake post',
+            'body'  => 'The body of this fake post',
+        ]);
+
+        $this->assertCount(1, Post::all());
+        $this->assertCount(1, $author->posts);
+
+        // Using tap() to alias $author->posts()->first() to $post
+        // To provide cleaner and grouped assertions
+        tap($author->posts()->first(), function ($post) use ($author) {
+            $this->assertEquals('My first fake post', $post->title);
+            $this->assertEquals('The body of this fake post', $post->body);
+            $this->assertTrue($post->author->is($author));
+        });
     }
 }
