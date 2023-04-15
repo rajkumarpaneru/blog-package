1a34502a58b7d34ce29a70c22c0f38a19cd64efc
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-15 14:20:38 +0545

    test for post's author type

--- tests/Unit/PostTest.php
+++ tests/Unit/PostTest.php
@@ -31,4 +31,11 @@ class PostTest extends TestCase
         $post = Post::factory()->create(['author_id' => 999]); // we choose an off-limits value for the author_id so it is unlikely to collide with another author_id in our tests
         $this->assertEquals(999, $post->author_id);
     }
+
+    /** @test */
+    function a_post_has_an_author_type()
+    {
+        $post = Post::factory()->create(['author_type' => 'Fake\User']);
+        $this->assertEquals('Fake\User', $post->author_type);
+    }
 }
