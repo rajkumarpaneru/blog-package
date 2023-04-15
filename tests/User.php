e430d130d71fc309c3c0d92e5cddd2c0e1ea85fb
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-15 14:00:32 +0545

    user model and factory for tests

---
+++ tests/User.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Raajkumarpaneru\BlogPackage\Tests;
+
+use Illuminate\Auth\Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\Access\Authorizable;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Raajkumarpaneru\BlogPackage\Traits\HasPosts;
+
+class User extends Model implements AuthorizableContract, AuthenticatableContract
+{
+    use HasPosts, Authorizable, Authenticatable, HasFactory;
+
+    protected $guarded = [];
+
+    protected $table = 'users';
+
+    protected static function newFactory()
+    {
+        return UserFactory::new();
+    }
+}
