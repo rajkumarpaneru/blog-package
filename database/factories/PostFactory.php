c4eb2d1aba5140c68eec7c30d127fb81bf113300
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-15 14:17:52 +0545

    modify post factory to add author_type

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,17 +4,31 @@ namespace Raajkumarapaneru\BlogPackage\Database\Factories;
 
 use Raajkumarpaneru\BlogPackage\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Raajkumarpaneru\BlogPackage\Tests\User;
 
 class PostFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
     protected $model = Post::class;
 
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
     public function definition()
     {
+        $author = User::factory()->create();
+
         return [
-            'title' => $this->faker->words(3, true),
-            'body' => $this->faker->paragraph,
-            'author_id' => 999,
+            'title'     => $this->faker->words(3, true),
+            'body'      => $this->faker->paragraph,
+            'author_id' => $author->id,
+            'author_type' => get_class($author)
         ];
     }
 }
