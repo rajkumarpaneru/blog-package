4137affb969f1b36d89ef4bc4b22cd51bab1a092
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-15 14:15:43 +0545

    user migration stub with test setup to run users migration

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -22,10 +22,11 @@ class TestCase extends \Orchestra\Testbench\TestCase
 
     protected function getEnvironmentSetUp($app)
     {
-        // import the CreatePostsTable class from the migration
         include_once __DIR__ . '/../database/migrations/create_posts_table.php.stub';
+        include_once __DIR__ . '/../database/migrations/create_users_table.php.stub';
 
-        // run the up() method of that migration class
-        (new CreatePostsTable)->up();
+        // run the up() method (perform the migration)
+        (new \CreatePostsTable)->up();
+        (new \CreateUsersTable)->up();
     }
 }
