0344a9a04139c77b9ba51843a9a19e1bb48768c4
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-16 17:04:10 +0545

    add test for post published notification

---
+++ tests/Unit/NotifyPostWasPublishedTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Raajkumarpaneru\BlogPackage\Tests\Unit;
+
+use Illuminate\Support\Facades\Notification;
+use Raajkumarpaneru\BlogPackage\Models\Post;
+use Raajkumarpaneru\BlogPackage\Notifications\PostWasPublishedNotification;
+use Raajkumarpaneru\BlogPackage\Tests\TestCase;
+use Raajkumarpaneru\BlogPackage\Tests\User;
+
+class NotifyPostWasPublishedTest extends TestCase
+{
+    /** @test */
+    public function it_can_notify_a_user_that_a_post_was_published()
+    {
+        Notification::fake();
+
+        $post = Post::factory()->create();
+
+        // the User model has the 'Notifiable' trait
+        $user = User::factory()->create();
+
+        Notification::assertNothingSent();
+
+        $user->notify(new PostWasPublishedNotification($post));
+
+        Notification::assertSentTo(
+            $user,
+            PostWasPublishedNotification::class,
+            function ($notification) use ($post) {
+                return $notification->post->id === $post->id;
+            }
+        );
+    }
+}
