5a33e8db3a0fca383ba798f475da67a695b53f98
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-16 20:58:58 +0545

    return full path in newFactory function

--- src/Models/Post.php
+++ src/Models/Post.php
@@ -4,22 +4,20 @@ namespace Raajkumarpaneru\BlogPackage\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Raajkumarapaneru\BlogPackage\Database\Factories\PostFactory;
 
 class Post extends Model
 {
     use HasFactory;
 
-    // Disable Laravel's mass assignment protection
     protected $guarded = [];
-
-    protected static function newFactory()
-    {
-        PostFactory::new();
-    }
 
     public function author()
     {
         return $this->morphTo();
+    }
+
+    protected static function newFactory()
+    {
+        return \JohnDoe\BlogPackage\Database\Factories\PostFactory::new();
     }
 }
