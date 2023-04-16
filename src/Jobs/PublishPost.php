1d9f81557a19d94a3cfab4038e6ee96f77d0f8a6
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-16 15:53:44 +0545

    job to publish post

---
+++ src/Jobs/PublishPost.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Raajkumarpaneru\BlogPackage\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Raajkumarpaneru\BlogPackage\Models\Post;
+
+class PublishPost implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    public function handle()
+    {
+        $this->post->publish();
+    }
+}
